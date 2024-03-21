3b343babf92be5a9d135dcbc19fb7d353199499f
tchinlapha <tchinlapha@gmail.com>
2024-03-22 00:25:48 +0700

    initial project

---
+++ src/app/(root)/page.jsx
@@ -0,0 +1,156 @@
+"use client";
+
+import {
+  Button,
+  Avatar,
+  Typography,
+  Card,
+  CardBody,
+  CardHeader,
+} from "@material-tailwind/react";
+
+export default function RootPage() {
+  return (
+    <>
+      <div className="container mx-auto min-h-screen">
+        {/* Header */}
+        <div className="flex items-center py-8">
+          <div className="w-4/12">
+            <div className="flex items-center gap-4">
+              <Avatar
+                src="https://docs.material-tailwind.com/img/face-2.jpg"
+                alt="avatar"
+              />
+              <div>
+                <Typography variant="h6">Tania Andrew</Typography>
+                <Typography
+                  variant="small"
+                  color="gray"
+                  className="font-normal"
+                >
+                  Web Developer
+                </Typography>
+              </div>
+            </div>
+          </div>
+          <div className="w-4/12 text-center">
+            <Typography variant="h1">One DD</Typography>
+          </div>
+          <div className="w-4/12 text-right">
+            <Button>Logout</Button>
+          </div>
+        </div>
+        {/* Menu */}
+        <div className="flex gap-3 justify-end items-center">
+          <Button>Button 1</Button>
+          <Button>Button 2</Button>
+          <Button>Button 3</Button>
+        </div>
+        {/* Story Card */}
+        <div className="mt-12 flex items-center">
+          <div className="w-6/12">
+            <Card className="w-full max-w-[48rem] flex-row">
+              <CardHeader
+                shadow={false}
+                floated={false}
+                className="m-0 w-2/5 shrink-0 rounded-r-none"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                  alt="card-image"
+                  className="h-full w-full object-cover"
+                />
+              </CardHeader>
+              <CardBody>
+                <Typography
+                  variant="h6"
+                  color="gray"
+                  className="mb-4 uppercase"
+                >
+                  startups
+                </Typography>
+                <Typography variant="h4" color="blue-gray" className="mb-2">
+                  Lyft launching cross-platform service this week
+                </Typography>
+                <Typography color="gray" className="mb-8 font-normal">
+                  Like so many organizations these days, Autodesk is a company
+                  in transition. It was until recently a traditional boxed
+                  software company selling licenses. Yet its own business model
+                  disruption is only part of the story
+                </Typography>
+                <div className="flex items-center gap-4">
+                  <Avatar
+                    src="https://docs.material-tailwind.com/img/face-2.jpg"
+                    alt="avatar"
+                  />
+                  <div>
+                    <Typography variant="h6">Tania Andrew</Typography>
+                    <Typography
+                      variant="small"
+                      color="gray"
+                      className="font-normal"
+                    >
+                      Web Developer
+                    </Typography>
+                  </div>
+                </div>
+              </CardBody>
+            </Card>
+          </div>
+          <div className="w-6/12">
+            <Card className="w-full max-w-[48rem] flex-row">
+              <CardHeader
+                shadow={false}
+                floated={false}
+                className="m-0 w-2/5 shrink-0 rounded-r-none"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80"
+                  alt="card-image"
+                  className="h-full w-full object-cover"
+                />
+              </CardHeader>
+              <CardBody>
+                <Typography
+                  variant="h6"
+                  color="gray"
+                  className="mb-4 uppercase"
+                >
+                  startups
+                </Typography>
+                <Typography variant="h4" color="blue-gray" className="mb-2">
+                  Lyft launching cross-platform service this week
+                </Typography>
+                <Typography color="gray" className="mb-8 font-normal">
+                  Like so many organizations these days, Autodesk is a company
+                  in transition. It was until recently a traditional boxed
+                  software company selling licenses. Yet its own business model
+                  disruption is only part of the story
+                </Typography>
+                <a href="#" className="inline-block">
+                  <Button variant="text" className="flex items-center gap-2">
+                    Learn More
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      strokeWidth={2}
+                      className="h-4 w-4"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+                      />
+                    </svg>
+                  </Button>
+                </a>
+              </CardBody>
+            </Card>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
